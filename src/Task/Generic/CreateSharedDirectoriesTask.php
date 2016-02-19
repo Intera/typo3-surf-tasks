7492de631942cc7acb2777739db277ed7d292b15
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-02-19 13:46:59 +0100

    [FEATURE] Add CreateSharedDirectoriesTask

---
+++ src/Task/Generic/CreateSharedDirectoriesTask.php
@@ -0,0 +1,50 @@
+<?php
+namespace Intera\Surf\Task\Generic;
+
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+use TYPO3\Surf\Domain\Model\Node;
+
+class CreateSharedDirectoriesTask extends \TYPO3\Surf\Domain\Model\Task implements \TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface
+{
+    use \TYPO3\Surf\Domain\Service\ShellCommandServiceAwareTrait;
+
+    /**
+     * Execute this task
+     *
+     * @param \TYPO3\Surf\Domain\Model\Node $node
+     * @param \TYPO3\Surf\Domain\Model\Application $application
+     * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+     * @param array $options
+     * @return void
+     */
+    public function execute(Node $node, Application $application, Deployment $deployment, array $options = array())
+    {
+        if (!isset($options['directories']) || !is_array($options['directories']) || $options['directories'] === array()) {
+            return;
+        }
+
+        $commands = array(
+            'cd ' . $application->getSharedPath()
+        );
+        foreach ($options['directories'] as $path) {
+            $commands[] = 'mkdir -p ' . $path;
+        }
+
+        $this->shell->executeOrSimulate($commands, $node, $deployment);
+    }
+
+    /**
+     * Simulate this task
+     *
+     * @param \TYPO3\Surf\Domain\Model\Node $node
+     * @param \TYPO3\Surf\Domain\Model\Application $application
+     * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+     * @param array $options
+     * @return void
+     */
+    public function simulate(Node $node, Application $application, Deployment $deployment, array $options = array())
+    {
+        $this->execute($node, $application, $deployment, $options);
+    }
+}
