555ae388698b226379f005d28522a26f7c7ed2f2
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-05-04 15:35:13 +0200

    [TASK] Cleanup Composer install task
    Remove duplicate code, add possiblity to add custom flags.

--- src/Task/Composer/InstallTask.php
+++ src/Task/Composer/InstallTask.php
@@ -1,69 +1,26 @@
 <?php
 namespace Intera\Surf\Task\Composer;
 
-use TYPO3\Surf\Domain\Model\Application;
-use TYPO3\Surf\Domain\Model\Deployment;
-use TYPO3\Surf\Domain\Model\Node;
-
 /**
  * Installs the composer packages based on a composer.json file in the projects root folder
  */
 class InstallTask extends \TYPO3\Surf\Task\Composer\InstallTask
 {
     /**
-     * Executes a composer install in the configured directory.
-     *
-     * @param Node $node
-     * @param Application $application
-     * @param Deployment $deployment
-     * @param array $options
-     * @throws \TYPO3\Surf\Exception\InvalidConfigurationException
-     */
-    public function execute(Node $node, Application $application, Deployment $deployment, array $options = [])
-    {
-        if (!empty($options['composerRootPath'])) {
-            $composerRootPath = $options['composerRootPath'];
-        } elseif (isset($options['useApplicationWorkspace']) && $options['useApplicationWorkspace'] === true) {
-            $composerRootPath = $deployment->getWorkspacePath($application);
-        } else {
-            $composerRootPath = $deployment->getApplicationReleasePath($application);
-        }
-
-        if (isset($options['nodeName'])) {
-            $node = $deployment->getNode($options['nodeName']);
-            if ($node === null) {
-                throw new \TYPO3\Surf\Exception\InvalidConfigurationException(sprintf('Node "%s" not found', $options['nodeName']), 1369759412);
-            }
-        }
-
-        if (isset($options['forceLocalMode']) && $options['forceLocalMode']) {
-            $node = $deployment->getNode('localhost');
-        }
-
-        if ($this->composerManifestExists($composerRootPath, $node, $deployment)) {
-            $command = $this->buildComposerInstallCommand($composerRootPath, $options);
-            $command .= ' --quiet';
-            $this->shell->executeOrSimulate($command, $node, $deployment);
-        }
-    }
-
-    /**
      * Build the composer command to "install --no-dev" in the given $path.
      *
      * @param string $manifestPath
      * @param array $options
-     * @return string
+     * @return array
      * @throws \TYPO3\Surf\Exception\TaskExecutionException
      */
-    protected function buildComposerInstallCommand($manifestPath, array $options)
+    protected function buildComposerInstallCommands($manifestPath, array $options)
     {
-        if (!isset($options['composerCommandPath'])) {
-            throw new \TYPO3\Surf\Exception\TaskExecutionException('Composer command not found. Set the composerCommandPath option.', 1349163257);
+        $command = parent::buildComposerInstallCommands($manifestPath, $options);
+        $composerFlags = isset($options['composerFlags']) ? $options['composerFlags'] . ' ' : '';
+        if (!empty($options['ignorePlatformRequirements'])) {
+            $composerFlags .= '--ignore-platform-reqs ';
         }
-        $arguments = '--no-ansi --no-interaction --no-dev --no-progress';
-        if (isset($options['ignorePlatformRequirements']) && $options['ignorePlatformRequirements']) {
-            $arguments .= ' --ignore-platform-reqs';
-        }
-        return sprintf('cd %s && %s install ' . $arguments, escapeshellarg($manifestPath), escapeshellcmd($options['composerCommandPath']));
+        return str_replace('2>&1', $composerFlags . '2>&1', $command);
     }
 }
